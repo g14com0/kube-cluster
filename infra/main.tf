3d3890634fe4f9b2dd6787b42d5b4edb0b54b99b
g14com0 <giacomo.furlan97@gmail.com>
2022-06-06 23:45:45 +0200

    test

--- infra/main.tf
+++ infra/main.tf
@@ -6,19 +6,11 @@ module "google_kubernetes_cluster" {
   source = "github.com/g14com0/kube-terraform//gke?ref=gkeDev"
 
   name               = "test"
-  location           = "us-central1"
   initial_node_count = 1
   project            = "gcfr-352018"
   remove_default_node_pool = false
-}
 
-terraform {
-  required_version = ">= 0.13.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.11.0"
-    }
-  }
+  regions = "europe-west3"
+  zone = ["europe-west3-b", "europe-west3-c"]
+  zonal = true
 }

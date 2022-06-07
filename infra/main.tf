2a9356704261cfdca17827bfabfd33834a5fcd33
g14com0 <giacomo.furlan97@gmail.com>
2022-06-08 00:07:50 +0200

    testing

--- infra/main.tf
+++ infra/main.tf
@@ -3,14 +3,19 @@ provider "google" {
 }
 
 module "google_kubernetes_cluster" {
-  source = "github.com/g14com0/kube-terraform//gke?ref=main"
+  source = "github.com/g14com0/kube-terraform//gke?ref=develop"
 
   name                     = "gcfr"
   project                  = "gcfr-352018"
-  remove_default_node_pool = true
+  remove_default_node_pool = false
 
   regions         = "europe-west3"
   zone            = ["europe-west3-a"]
   zonal           = false
-  node_pools_name = ["node1", "node2"]
+  node_pools = [
+    {
+      machine_type              = "e2-medium"
+      image_type                = "COS_CONTAINERD"
+    },
+  ]
 }

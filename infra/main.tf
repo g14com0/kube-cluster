b99c475617db890e8acad06fae8bbba9dac70243
g14com0 <giacomo.furlan97@gmail.com>
2022-06-07 21:52:31 +0200

    fmt + changed branch

--- infra/main.tf
+++ infra/main.tf
@@ -3,14 +3,14 @@ provider "google" {
 }
 
 module "google_kubernetes_cluster" {
-  source = "github.com/g14com0/kube-terraform//gke?ref=main"
+  source = "github.com/g14com0/kube-terraform//gke?ref=secfix"
 
   name                     = "gcfr"
   project                  = "gcfr-352018"
   remove_default_node_pool = true
 
-  regions = "europe-west3"
-  zone    = ["europe-west3-a"]
-  zonal   = false
+  regions         = "europe-west3"
+  zone            = ["europe-west3-a"]
+  zonal           = false
   node_pools_name = ["node1", "node2"]
 }

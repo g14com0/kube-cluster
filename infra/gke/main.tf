bff74265481e69977c793d0a007bd95f6a18956e
g14com0 <giacomo.furlan97@gmail.com>
2022-06-25 14:13:10 +0200

    test

--- infra/gke/main.tf
+++ infra/gke/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 module "google_kubernetes_cluster" {
   source = "github.com/g14com0/terraform-modules//gke?ref=main"
 
-  name                     = "gcfr-2"
+  name                     = "gcfr-23"
   project                  = "gcfr-352018"
   remove_default_node_pool = false
 
